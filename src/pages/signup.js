852fd142cb97bd3af9ee6e6237368a519cd93448
Sushant Baweja <bawejasushant07@gmail.com>
2021-08-04 22:42:49 +0530

    commit

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,14 +4,14 @@ import axios from "axios";
 const SignUp = () => {
 
     const url = "http://localhost:5000/api/file/upload"; // backend url
+    const userUrl = "http://localhost:5000/api/users/signUp"
     const [selectedFile, setSelectedFile] = useState(null);
-    const [userInfo, setuserInfo] = useState([]);
+    const [userInfo, setuserInfo] = useState({name: "", email: "", password: "", confirmpassword: ""});
 
     const onChangeHandler = (event) => {
         console.log(event.target.files[0])
         let img = event.target.files[0];
-        setSelectedFile(URL.createObjectURL(img));
-        //console.log(selectedFile)
+        setSelectedFile(URL.createObjectURL(img));        
         const fd = new FormData();
         fd.append('productImage', event.target.files[0], event.target.files[0].name);
         axios.post(url, fd, {
@@ -22,41 +22,46 @@ const SignUp = () => {
 
     }
 
+    const onChangeUserHandle = (event) => {
+        const newuserInfo = { ...userInfo };
+        newuserInfo[event.target.name] = event.target.value;
+        setuserInfo(newuserInfo);   
+        console.log(userInfo);    
+
+    }
+
     const onClickUserHandler = (event) => {
         event.preventDefault();
-       console.log(event.target.files[0]);
-       
-        // const fd = new FormData();
-        // fd.append('productImage', selectedFile, selectedFile.name);
-        // axios.post(url, fd, {
-        //     onUploadProgress: progressEvent => console.log("uploaded progress: " + (progressEvent.loaded / progressEvent.total * 100) + "%")
-        // }).then(res => {
-        //     console.log(res);
-        // });
+        console.log(userInfo);  
+        axios.post(userUrl, userInfo, {
+            onUploadProgress: progressEvent => console.log("uploaded progress: " + (progressEvent.loaded / progressEvent.total * 100) + "%")
+        }).then(res => {
+            console.log("Submited SuccessFully");
+        });
+
     }
 
     return (
         <>
-
             <img src={selectedFile} alt="img" />
             <label htmlFor="imgfile">Upload :
             </label>
             <input type="file" name="productImage" onChange={onChangeHandler} />
 
-            <form >
+            <form onChange={onChangeUserHandle}>
                 <label htmlFor="name">Name:
                 </label>
-                <input type="text" name="name" />
+                <input type="text" required name="name" defaultValue={userInfo.name} />
                 <label htmlFor="email">Email:
                 </label>
-                <input type="text" name="email" />
+                <input type="text" required name="email" defaultValue={userInfo.email} />
                 <label htmlFor="password">Password:
                 </label>
-                <input type="text" name="password" />
+                <input type="password" required name="password" defaultValue={userInfo.password} />
                 <label htmlFor="confirmPassword">Confir Password:
                 </label>
-                <input type="text" name="confirmpassword" />
-                <input type="button" onClick={onClickUserHandler} />
+                <input type="text" required name="confirmpassword" defaultValue={userInfo.confirmpassword} />
+                <input type="button" onClick={onClickUserHandler} value="submit" />
             </form>
         </>
     );
